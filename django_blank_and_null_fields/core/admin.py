ada119d4cbc0d2f1488563e7dd70aa8a358b009f
Samuel <fg.samuel@gmail.com>
2021-10-30 17:32:16 -0300

    create models
    Create models and configure admin

--- django_blank_and_null_fields/core/admin.py
+++ django_blank_and_null_fields/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django_blank_and_null_fields.core.models import Exemplo
+
+
+class ExemploAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'campo_1', 'campo_2', 'campo_3', 'campo_4')
+
+
+admin.site.register(Exemplo, ExemploAdmin)
--- django_blank_and_null_fields/core/models.py
+++ django_blank_and_null_fields/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Exemplo(models.Model):
+    descricao = models.CharField("descrição", max_length=30)
+    campo_1 = models.CharField(max_length=10)                           # blank=False,  null=False
+    campo_2 = models.CharField(max_length=10, blank=True)               # blank=True,   null=False
+    campo_3 = models.CharField(max_length=10, null=True)                # blank=False,  null=True
+    campo_4 = models.CharField(max_length=10, blank=True, null=True)    # blank=False,  null=False
+
+    def __str__(self):
+        return self.descricao
