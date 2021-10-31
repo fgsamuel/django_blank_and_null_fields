66e40310c955a44c4b79b0f169af5605216dd8b5
Samuel <fg.samuel@gmail.com>
2021-10-30 22:02:18 -0300

    Change fields to integer

--- django_blank_and_null_fields/core/models.py
+++ django_blank_and_null_fields/core/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Exemplo(models.Model):
     descricao = models.CharField("descrição", max_length=30)
-    campo_1 = models.CharField(max_length=10)                           # blank=False,  null=False
-    campo_2 = models.CharField(max_length=10, blank=True)               # blank=True,   null=False
-    campo_3 = models.CharField(max_length=10, null=True)                # blank=False,  null=True
-    campo_4 = models.CharField(max_length=10, blank=True, null=True)    # blank=False,  null=False
+    campo_1 = models.IntegerField(default=10)             # blank=False,  null=False
+    campo_2 = models.IntegerField(blank=True, null=True)  # blank=True,   null=True
+    campo_3 = models.IntegerField(null=True)              # blank=False,  null=True
+    campo_4 = models.IntegerField(blank=True)             # blank=True,   null=False
 
     def __str__(self):
         return self.descricao
